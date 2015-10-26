a3a54eb298acf2e87ae7c8f8f9651737b38daf6b
luca wyss <wyss.luca@gmail.com>
2015-10-26 19:20:29 +0100

    integration of the mockup based on materializecss

---
+++ scripts/main.js
@@ -0,0 +1,65 @@
+$( document ).ready(function(){
+			
+	$('.button-collapse').sideNav({closeOnClick: true});
+	$('.side-nav .close').sideNav('hide');
+		      
+	$('.parallax').parallax();
+	
+	$(".dropdown-button").dropdown({hover: true,belowOrigin: true});       
+	
+	$('.team article').click(function() {
+		$(this).find('.card-panel').toggleClass('z-depth-2');
+		$('.team article').not(this).find('.card-panel').removeClass('z-depth-2');
+		var target = $(this).data("target");
+		$('#teamContent #' + target).toggleClass('hidden');
+		$('#teamContent').children().not( '#' + target).addClass('hidden');
+	});
+	
+});
+
+// Changing the header menu on scroll
+
+var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
+
+function scrollHeader() {
+    // Has scrolled class on header
+    var zvalue = $(document).scrollTop();
+    if ( zvalue > 75 )
+        $("#header, #lang").addClass("scrolled");
+    else
+        $("#header, #lang").removeClass("scrolled");
+}
+
+jQuery(document).ready(function($){
+
+    // ON SCROLL EVENTS
+    if (!isTouch){
+        $(document).scroll(function() {
+            scrollHeader();
+        });
+    };
+
+    // TOUCH SCROLL
+    $(document).on({
+        'touchmove': function(e) {
+            scrollHeader(); // Replace this with your code.
+        }
+    });
+});
+
+
+// Smotthing the scroll on Anchors
+$(function() {
+  $('#header a[href*=#]:not([href=#]), .showcase a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-60
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
